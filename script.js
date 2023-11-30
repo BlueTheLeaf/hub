4c7ddde036ce23f06422fb10a5a1edc52aeced8e
BlueTheLeaf <152555501+BlueTheLeaf@users.noreply.github.com>
2023-11-30 20:55:46 +0000

    Git init

---
+++ script.js
@@ -0,0 +1,19 @@
+const formEl = document.querySelector("form")
+const usernameEl = document.getElementById("username")
+const passwordEl = document.getElementById("password")
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const username = usernameEl.value
+    const password = passwordEl.value
+
+    const defaultUser = {
+        username: "user1",
+        password: "password1"
+    }
+
+    if (username !== defaultUser.username || password !== defaultUser.password) return
+
+    window.location.replace("https://jsahdjasnduaci7yacinajkscjna8scnakj2n5kadskjnas87cnas-cklana.schoollocked.repl.co/")
+})
